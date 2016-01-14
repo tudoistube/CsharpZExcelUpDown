fd4505ee09b0577e035430b94ea9f4ee2b97d338
2Be <tudoistube@ymail.com>
2016-01-14 19:10:12 +0900

    변경작업 on master

--- ZExcelUpDown/Form1.Designer.cs
+++ ZExcelUpDown/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.btnXlsUp = new System.Windows.Forms.Button();
             this.btnXlsDown = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnXlsUp
@@ -60,11 +61,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Git을 이용한 엑셀업다운로드 개발중!";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Git을 이용한 엑셀업다운로드 개발중!";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnXlsDown);
             this.Controls.Add(this.btnXlsUp);
@@ -80,6 +91,7 @@
         private System.Windows.Forms.Button btnXlsUp;
         private System.Windows.Forms.Button btnXlsDown;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
